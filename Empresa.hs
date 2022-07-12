02bbdacfd31a7ad1f784fe722f97c07b42d35c4c
GabrielLacerda00 <gabriel.lacerd.gr@gmail.com>
2022-07-11 21:14:46 -0300

    criando cadastro de um funcionário

---
+++ Empresa.hs
@@ -0,0 +1,67 @@
+import Funcionario
+import Util
+import Mensagens
+import System.IO
+import Data.List
+
+
+--- Chama uma das funcionalidades do menu da empresa
+menuEmpresa:: (IO()) -> IO ()
+menuEmpresa menu = do
+                Mensagens.menuEmpresa
+                funcionalidade <- Util.lerEntradaString
+
+                if funcionalidade == "1"
+                    then do cadastroDeFuncionario menu
+                else if funcionalidade == "2"
+                    then do {}
+                else if funcionalidade == "3"
+                    then do {}
+                else if funcionalidade == "4"
+                    then do {}
+                else if funcionalidade == "5"
+                    then do {}
+                else if funcionalidade == "6"
+                    then do {}
+                else if funcionalidade == "7"
+                    then do {}
+                else if funcionalidade == "8"
+                    then do {}
+                else if funcionalidade == "9"
+                    then do {}
+                else if funcionalidade == "10"
+                  then do {}
+                else if funcionalidade == "11"
+                  then do {}
+                else if funcionalidade == "12"
+                  then do {}
+                else if funcionalidade == "13"
+                  then do {}
+                else if funcionalidade == "14"
+                  then do {}
+                else if funcionalidade == "15"
+                  then do menu
+                else do
+                  {{putStrLn("\nError: OPÇÃO INVÁLIDA\n"); }
+
+                
+
+-- Cadastrado de funcionario na empresa
+cadastroDeFuncionario:: (IO()) -> IO()
+cadastroDeFuncionario menu = do
+                Mensagens.cadastrarNome
+                nome <- Util.lerEntradaString
+
+                Mensagens.getCpf
+                cpf <- Util.lerEntradaString
+
+                arquivo <- readFile "arquivos/funcionarios.txt"
+                let lista = ((Data.List.map (Util.wordsWhen(==',') ) (lines arquivo)))
+
+                if (Util.temCadastro cpf lista)
+                    then do {Mensagens.usuarioCadastrado, cadastroDeFuncionario menu}
+                else do
+                    let funcionarioString = cpf ++ "," ++ nome ++ "\n"
+                    appendFile "arquivos/funcionarios.txt" (funcionarioString)
+
+
--- Mensagens.hs
+++ Mensagens.hs
@@ -18,6 +18,7 @@ mensagemSaida :: IO ()
 mensagemSaida =
     putStr("\nAté mais!\n")
 
+
 usuarioNaoCadastrado :: IO()
 usuarioNaoCadastrado =
     putStrLn("\nErro: usuário não cadastrado!\n")
@@ -60,3 +61,33 @@ valorPago :: String -> [[String]] -> IO()
 valorPago cpf lista2 = do
 
     putStr("O valor a ser pago em REAIS pelo cliente " ++ Util.getNome cpf lista2 ++ " é: ")
+
+menuEmpresa :: IO()
+menuEmpresa = do
+    putStrLn("\n       -----Menu da Empresa-----")
+
+    putStrLn("\nComo deseja prosseguir?")
+    putStrLn("[1] Cadastrar funcionário")
+    putStrLn("[2] Alterar funcionário")
+    putStrLn("[3] Excluir funcionário")
+    putStrLn("[4] Visualizar funcionários ativos")
+    putStrLn("[5] Listar assentos executivos e econômicos disponíveis")
+    putStrLn("[6] Listar assentos executivos e econômicos indisponíveis")
+    putStrLn("[7] Criar valores para os assentos")
+    putStrLn("[8] Alterar valores dos assentos")
+    putStrLn("[9] Deletar valores dos assentos")
+    putStrLn("[10] Listar valores para cada tipo de assento")
+    putStrLn("[11] Criar descontos")
+    putStrLn("[12] Alterar descontos")
+    putStrLn("[13] Excluir descontos")
+    putStrLn("[14] Ver valor em caixa")
+    putStrLn("[15] Voltar ao menu principal\n")
+
+cadastradoEfetuado :: IO()
+cadastradoEfetuado = do
+    putStr("\nCADASTRADO EFETUADO COM SUCESSO!")
+
+cadastrarNome :: IO()
+cadastrarNome = do
+    putStrLn("\n       -----CADASTRO DE USUÁRIO-----")
+    putStr("\nInforme o nome: ")
